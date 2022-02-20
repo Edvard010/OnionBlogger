df51f948c11349ab258e0673bb16f0bcee26ed2f
Edvard010 <piszczatowski.grzegorz@gmail.com>
2022-02-20 17:50:07 +0100

    Domain, Entities, Interfaces, Infrastructure

---
+++ Domain/Entities/Post.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Post : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public Post() { }
+        public Post(int id, string title, string content)
+        {
+            (Id, Title, Content) = (id, title, content);            
+        }
+    }
+}
---
+++ Domain/Interfaces/IPostRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public interface IPostRepository
+    {
+        IEnumerable<Post> GetAll();
+        Post GetById(int id);
+        Post Add(Post post);
+        void Update(Post post);
+        void Delete(Post post);
+    }
+}
